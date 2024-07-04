a24651b296914c9892d5c3dae52f5ae005ac7b43
Trinadh Machanavazzala <trinadhm469@gmail.com>
2024-07-04 14:50:38 -0500

    geoLocation API

---
+++ mapAPI/script.js
@@ -0,0 +1,126 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+let map;
+let mapEvent;
+
+class Workout{
+    date = new Date();
+    id = (Date.now()+ '').slice(-10);
+    constructor(coords,distance,duration){
+        this.coords = coords;
+        this.distance = distance; //in km
+        this.duration = duration; //in min
+    }
+}
+
+class Running extends Workout{
+
+    constructor(coords,distance,duration,cadence){
+        super(coords,distance,duration);
+        this.cadence = cadence;
+        this.calcPace();
+    }
+    calcPace(){
+        //mins/dis
+        this.pace = this.duration / this.distance;
+        return this.pace;
+    }
+}
+
+class Cycling extends Workout{
+
+    constructor(coords,distance,duration,elevationGain){
+        super(coords,distance,duration);
+        this.elevationGain = elevationGain;
+        this.calcSpeed();
+    }
+    calcSpeed(){
+        //mins/dis
+        this.speed = this.distance / this.duration;
+        return this.speed;
+    }
+}
+
+const run1 = new Running([39,-12],5.2,24,178);
+const cycle1 = new Cycling([39,-12],27,95,523);
+
+console.log(run1,cycle1);
+
+/////////////////////////////
+//Application Architecture
+////////////////////////////
+class App{
+    #map;
+    #mapEvent;
+
+    constructor(){
+ this._getPosition();
+      form.addEventListener('submit', this._newWorkout.bind(this))
+      inputType.addEventListener('change', this._toggleElevationField ) 
+    }
+
+    _getPosition(){
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function () {
+                    alert(`Could not fetch the current location`)
+                })
+        }
+    }
+    _loadMap(position){
+            const {latitude} = position.coords;
+            const {longitude} = position.coords;
+            console.log(`https://www.google.com/maps/@${latitude},${longitude},15z?entry=ttu`);
+            const cords = [latitude, longitude]
+            this.#map = L.map('map').setView(cords, 13);
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(this.#map);
+            // L.marker(cords).addTo(map).bindPopup(' A pretty css Map').openPopup();
+console.log(this);
+            //Adding the functionality if we click on the any location in the map
+            this.#map.on('click', this._showForm.bind(this))
+        }
+
+    _showForm(mapE){
+            this.#mapEvent = mapE
+            form.classList.remove('hidden');
+            inputDistance.focus();
+    }
+
+    _toggleElevationField() {
+        //Hidden Form Active if we are cycling
+      
+            inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+            inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    }
+
+    _newWorkout(e){
+        e.preventDefault();
+               // Clearing the form
+               inputDuration.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+               const { lat, lng } = this.#mapEvent.latlng;
+               L.marker([lat, lng]).addTo(this.#map).bindPopup(L.popup({
+                   maxWidth: 250,
+                   minWidth: 100,
+                   autoClose: false,
+                   closeOnClick: false,
+                   className: 'running-popup'
+               }))
+                   .setPopupContent('Work out')
+                   .openPopup();
+    }
+}
+
+const app = new App();
+//Display Marker
